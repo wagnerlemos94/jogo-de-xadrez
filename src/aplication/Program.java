396e557d08f57280f40053946cd41095c999e782
wagner lemos <wagner.lemos94@hotmail.com>
2020-01-04 21:38:19 -0300

    posição

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,11 +1,14 @@
 package aplication;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-			System.out.println("Hello Word");
-		
+			
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 
 	}
 
