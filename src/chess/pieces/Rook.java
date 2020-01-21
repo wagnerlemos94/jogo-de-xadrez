fbcb1a8ff3cfe33046aa2d0e92e535933573ea38
wagner lemos <wagner.lemos94@hotmail.com>
2020-01-21 20:55:19 -0300

    Correção de logica da torre

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -38,7 +38,7 @@ public class Rook extends ChessPiece {
 		p.setValues(position.getRow(), position.getColumn() - 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setRow(p.getColumn() - 1);
+			p.setColumn(p.getColumn() - 1);
 		}
 		if (getBoard().positionExists(p) && isThereOpponentPice(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
@@ -49,7 +49,7 @@ public class Rook extends ChessPiece {
 		p.setValues(position.getRow(), position.getColumn() + 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setRow(p.getColumn() + 1);
+			p.setColumn(p.getColumn() + 1);
 		}
 		if (getBoard().positionExists(p) && isThereOpponentPice(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
